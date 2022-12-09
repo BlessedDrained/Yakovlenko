7340879215bb2453f53c599517ab1afa48d6cff7
BlessedDrained <92720096+BlessedDrained@users.noreply.github.com>
2022-12-09 19:25:40 +0500

    Добавлено решение задания 3.2.1

---
+++ 3.2.1/3.2.1.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from datetime import datetime
+
+
+pd.set_option("expand_frame_repr", False)
+
+
+if __name__ == "__main__":
+    file_name = input("Введите имя файла: ")
+    df = pd.read_csv(file_name)
+    df["years"] = df["published_at"].apply(lambda s: datetime.strptime(s, "%Y-%m-%dT%H:%M:%S%z").year)
+    years = df["years"].unique()
+    for year in years:
+        data = df[df["years"] == year]
+        data.iloc[:, :6].to_csv(rf"csv_files\part_{year}.csv", index=False)
+
+
+
+
+
+
